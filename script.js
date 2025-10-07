a299d00127d748e534d10239cf35fc6c252eb6fc
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-07 09:24:48 +0530

    Add Tailwind CSS script and initialize JavaScript variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log(45*2-10);
+
+
+// const date = new Date();
+// console.log(date.getFullYear());
+
+
+// const first = "Hitendra"; 
+// const last = "Singh";
+// console.log(`${first} ${last}`);
+
+
+var a=12;
+console.log(a);
+var a=32;
+console.log(a);
+
