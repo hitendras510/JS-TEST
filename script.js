ca776f33dbfc371e61d079bac47dfd61821230ca
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-07 09:42:36 +0530

    Refactor script.js to comment out code examples for clarity

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//1.basics
 // console.log(45*2-10);
+
 
 
 // const date = new Date();
 // console.log(date.getFullYear());
+
 
 
 // const first = "Hitendra"; 
@@ -10,8 +13,43 @@
 // console.log(`${first} ${last}`);
 
 
-var a=12;
-console.log(a);
-var a=32;
-console.log(a);
+
+// var a=12;
+// console.log(a);
+// var a=32;
+//  console.log(a);
+
+
+
+// var a = 12*12;
+// // console.log(a);
+// console.log(typeof a);
+
+
+
+
+// var age = 20;
+// if (age>18) {
+    
+//     console.log("you're allowed");
+// } else {
+//     console.log("its not");
+// }
+
+
+
+//2.var and data types 
+
+// const pi = Math.PI;
+// console.log(pi);
+
+
+
+
+// let a = 12;
+// a = 32; //reassign
+// console.log(a);
+
+
+
 
