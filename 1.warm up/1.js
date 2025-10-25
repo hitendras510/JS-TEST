26dfa46e884cb5fb157e499ca5f25a6508d20b92
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-25 15:23:13 +0530

    new

---
+++ 1.warm up/1.js
@@ -0,0 +1,9 @@
+//BY SCHOOL4u
+function getLastElement(LocalArr) {
+    return LocalArr[LocalArr.length -1]
+}
+
+let arr = [2,1,5,6,87,7,8,9];
+
+let lastElement = getLastElement(arr)
+console.log(lastElement)
