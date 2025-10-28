6386a0efe0f59c064f6b17558e7d8bf6fea20c1c
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-28 12:07:31 +0530

    hi

--- 1.warm up/Q.js
+++ 1.warm up/Q.js
@@ -1,9 +1,21 @@
 //BY SCHOOL4u
-function getLastElement(LocalArr) {
-    return LocalArr[LocalArr.length -1]
-}
+//1.
+//  function getLastElement(LocalArr) {
+//     return LocalArr[LocalArr.length -1]
+// }
 
-let arr = [2,1,5,6,87,7,8,9];
+// let arr = [2,1,5,6,87,7,8,9];
 
-let lastElement = getLastElement(arr)
-console.log(lastElement)
+// let lastElement = getLastElement(arr)
+// console.log(lastElement)
+
+//2.
+// let arr1 = [2,3,4,5,6];
+// let arr2 = [4,5,7,8,9];
+
+// let combinedArr = arr1.concat(arr2)
+// console.log(combinedArr);
+
+//3.
+// let arr
+
