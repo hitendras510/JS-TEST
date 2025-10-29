ad09cf9bce7dba345f74164ada41b4c8af787730
Hitendra singh <hitendrasingh5489@gmail.com>
2025-10-29 22:08:46 +0530

    new

--- 1.warm up/Q.js
+++ 1.warm up/Q.js
@@ -1,5 +1,5 @@
 //BY SCHOOL4u
-//1.
+//1.return last element of an array
 //  function getLastElement(LocalArr) {
 //     return LocalArr[LocalArr.length -1]
 // }
@@ -9,13 +9,13 @@
 // let lastElement = getLastElement(arr)
 // console.log(lastElement)
 
-//2.
+//2. find combination of two array
 // let arr1 = [2,3,4,5,6];
 // let arr2 = [4,5,7,8,9];
 
 // let combinedArr = arr1.concat(arr2)
 // console.log(combinedArr);
 
-//3.
-// let arr
+//3. random integer btw 0 to 18
+
 
