4154cd141cbf242b2817afac8e6b57a61a089826
Hitendra singh <hitendrasingh5489@gmail.com>
2025-12-05 22:56:22 +0530

    . 

--- JS everything/script.js
+++ JS everything/script.js
@@ -6,14 +6,20 @@
 // console.log(b); // reference error becoz let respects block
 
 //q1 EARLY RETURN PATTERN
-function getGrade(score){
-    if(score >= 90 && score<=100) return 'A';
-    if(score >= 80 && score<=89) return 'B';
-    if(score >= 70 && score<=79) return 'C';
-    if(score >= 60 && score<=69) return 'D';
-    if(score >= 33 && score<=59) return 'E';
-    if(score >= 0 && score<=32) return 'A';
-    return "Invalid marks";
-}
+// function getGrade(score){
+//     if(score >= 90 && score<=100) return 'A';
+//     if(score >= 80 && score<=89) return 'B';
+//     if(score >= 70 && score<=79) return 'C';
+//     if(score >= 60 && score<=69) return 'D';
+//     if(score >= 33 && score<=59) return 'E';
+//     if(score >= 0 && score<=32) return 'A';
+//     return "Invalid marks";
+// }
 
-console.log(getGrade(54));
+// console.log(getGrade(54));
+
+//q2, print even num using for loop
+
+for(i=0; i<=10;i++){
+    if(i%2==0) console.log(i);
+}
